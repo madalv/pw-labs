afd50df0ab47beb3d9e47b8354530f714c153094
madalv <vlada.magal@isa.utm.md>
2023-05-23 21:17:33 +0300

    finished lab 5

--- README.md
+++ README.md
@@ -4,5 +4,6 @@
 - Lab 2 - go2web. Created a simple CLI app which displays scrapes info on a given website and displays it in the terminal.
 - Lab 3 - To-Do List. A simple to-do list with the abilities to add, complete, delete tasks and set notifications.
 - Lab 4 - Quiz App. Written in Vue & using Tailwind. Consumes data from a given API.
+- Lab 5 - Telegram Bot. Written Python (Flask), simply sends back 5 NYT articles on the required topic.
 
 Done by Magal Vlada, FAF-203.
--- tg-bot/app.py
+++ tg-bot/app.py
@@ -1,44 +1,74 @@
-from flask import Flask,request
+from flask import Flask, request
 import requests
-import sys
+import os
+from dotenv import load_dotenv
 
-key = "6103168544:AAFRuElMAnClGeqQ6KhIvMgCbNZ29xMYtCE"
+load_dotenv()
+
+key = os.getenv("TG_KEY")
+news_key = os.getenv("NEWS_KEY")
+news_url = os.getenv("NEWS_URL")
+webhook_url = os.getenv("WEBHOOK_URL")
 
 app = Flask(__name__)
 
-def sendmessage(chatid):
+def get_news(topic):
+    querystring = {
+        "api-key": news_key} if topic == "" else {"api-key": news_key, "q": topic}
+    response = requests.get(news_url, params=querystring)
+    data = response.json()
+    print(data["response"]["docs"][0])
+    return data["response"]["docs"]
+
+
+def sendmessage(chatid, text):
     url = "https://api.telegram.org/bot{}/sendMessage".format(key)
     payload = {
-        "text":"heyy",
-        "chat_id":chatid
-        }
-    
-    resp = requests.get(url,params=payload)
+        "text": text,
+        "chat_id": chatid
+    }
 
-@app.route("/",methods=["POST","GET"])
+    requests.get(url, params=payload)
+
+
+@app.route("/", methods=["POST", "GET"])
 def index():
-    if(request.method == "POST"):
-       
+    if (request.method == "POST"):
+
         resp = request.get_json()
         msgtext = resp["message"]["text"]
-        sendername = resp["message"]["from"]["first_name"]
         chatid = resp["message"]["chat"]["id"]
-        sendmessage(chatid)
+
+        split = msgtext.split()
+
+        print(split)
+
+        if (split[0] == '/start'):
+            sendmessage(chatid, "Hello.")
+        elif (split[0] == '/latest_news'):
+            news = get_news(msgtext.split()[1])
+            for i in range(5):
+                sendmessage(chatid, news[i]["web_url"])
+        else:
+            sendmessage(chatid, "Type /latest_news for the latest news.")
+
         return "Done"
     else:
-        return "hi"
+        return "get"
+
 
 @app.route("/setwebhook/")
 def setwebhook():
-    url = "https://4b31-109-185-158-95.ngrok-free.app"
-    s = requests.get("https://api.telegram.org/bot{}/setWebhook?url={}".format(key,url))
-    
+    s = requests.get(
+        "https://api.telegram.org/bot{}/setWebhook?url={}".format(key, webhook_url))
+
     print(s)
 
     if s:
         return "yes"
     else:
         return "fail"
-        
+
+
 if __name__ == "__main__":
     app.run(debug=True)
